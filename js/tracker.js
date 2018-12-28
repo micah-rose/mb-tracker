f4d9179252a7d6fa6c69981f5589bffc40cbbac8
micah-rose <micah.r.lund@gmail.com>
2018-12-28 14:41:39 -0700

    added basic functionality, discovered that I will need some control variables so counts don't go above what the user inputs nor below zero. Updated to do lists for functions as well

--- js/tracker.js
+++ js/tracker.js
@@ -1,15 +1,17 @@
 //TO DO's:
-//Take in user input for number to track
-//Display input for # Remaining
-//Add onclick functionality to count down from number input
-//Add functionality to Start Over and Oops nav links (increase number input for accidental clicks)
-//Display motivational quote and change every time app loads (pull from an API?)
+//Add functionality to Start Over nav link
+//Display/update motivational quote every time app loads (pull from an API?)
 //Add functionality for when goal is met (TBD)
 
 //HTML TO DO's:
 //Create a page for first time load to take in the user input, but save info
 //so the user doesn't lose their count when the app is closed
-//Add to anchor tags when ready to apply functions - <a href="javascript:someFunction()">LINK</a>
+
+
+//change the below to an input form that displays the message and ollects the data
+//var message = alert("Hello!! Welcome to the MB Tracker, here you can set how many times you want to do a task or goal and keep track of your remaining times to do it.");
+//var goal = prompt("Please tell us how many times you want to perform your task or goal by inputting a numeric value: ");
+var goal = 50; //created for testing functionality
 
 var today = new Date();
 var dd = today.getDate();
@@ -18,9 +20,12 @@ var yyyy = today.getFullYear();
 var newYear = new Date(today.getFullYear(), 11, 31);
 var daysLeft;
 
+//runs setDate and setDaysLeft functions, also updates # remaining display
 function updateTop(){
     setDate();
     setDaysLeft();
+    document.getElementById("remaining").innerText = "# Remaining out of " + goal;
+    document.getElementById("user-input").innerText = goal;
 }
 
 //updates to display current date
@@ -48,4 +53,18 @@ function setDaysLeft(){
     var days = 1000*60*60*24
     daysLeft = Math.ceil((newYear.getTime()-today.getTime())/(days));
     document.getElementById("days-left").innerText = "There are " + daysLeft + " days left in the year!!";
+}
+
+//creates functionality for when user clicks to decrement their goal count
+//TO DO: create control variable so clicks can't go below zero
+function updateGoal(){
+    goal--;
+    document.getElementById("user-input").innerText = goal;
+}
+
+//creates function for Oops link
+//TO DO: create control variable so clicks can't go over the set goal
+function oopsClick(){
+    goal++;
+    document.getElementById("user-input").innerText = goal;
 }
