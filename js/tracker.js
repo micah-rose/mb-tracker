cce4d9b4af97f538cfe1d29b87630e65c7487a76
micah-rose <micah.r.lund@gmail.com>
2018-12-28 12:47:43 -0700

    modified more of the design and user interface, made list of JavaScript to do's that sparked some more ideas for the interface that I will continue to play around with

---
+++ js/tracker.js
@@ -0,0 +1,14 @@
+//TO DO's:
+//Take in user input for number to track
+//Display input for # Remaining
+//Add onclick functionality to count down from number input
+//Display today's date and how many days are left in the year
+//Add functionality to Oops button (increase number input for accidental clicks)
+//Display motivational quote and change every time app loads (pull from an API?)
+//Add functionality for when goal is met (TBD)
+
+//HTML TO DO's:
+//Create a page for first time load to take in the user input, but save info
+//so the user doesn't lose their count when the app is closed
+//*MAYBE* - Add a button to start over at any time? Or instead of buttons, add
+//headers at the top??
